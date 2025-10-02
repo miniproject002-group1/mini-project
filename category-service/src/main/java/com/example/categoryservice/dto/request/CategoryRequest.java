6eebeeb2bf4951640cacf7bb43c7c921c50eda6f
ZuuKie <zuukie333@gmail.com>
2025-10-02 14:15:21 +0700

    update CategoryRequest

--- category-service/src/main/java/com/example/categoryservice/dto/request/CategoryRequest.java
+++ category-service/src/main/java/com/example/categoryservice/dto/request/CategoryRequest.java
@@ -1,6 +1,8 @@
 package com.example.categoryservice.dto.request;
 
 import com.example.categoryservice.entity.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Setter
@@ -11,7 +13,9 @@ import lombok.*;
 @ToString
 public class CategoryRequest {
 
+    @NotBlank
     private String name;
+
     private String description;
 
     public Category toEntity() {
